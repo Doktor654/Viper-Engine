84fe5f9e71aec06c00e1867b7f84a4ffef563058
Doktor <74203521+Doktor654@users.noreply.github.com>
2026-04-16 14:44:12 +0200

    Update Input.py to support Mouse events

--- MyEngine/engine/input.py
+++ MyEngine/engine/input.py
@@ -5,12 +5,18 @@ class Input:
         self.current_keys = None
         self.previous_keys = None
 
+        self.current_mouse = None
+        self.previous_mouse = None
     def update(self):
+        ## Keys
         # Spara gamla state
         self.previous_keys = self.current_keys
-
         # Hämta nya
         self.current_keys = pygame.key.get_pressed()
+
+        # Mouse
+        self.previous_mouse = self.current_mouse
+        self.current_mouse = pygame.mouse.get_pressed()
 
     def key_pressed(self, target_key):
         if self.current_keys[target_key]:
@@ -22,3 +28,15 @@ class Input:
         if self.current_keys[target_key] and not self.previous_keys[target_key]:
             return True
         return False
+
+    
+    ## Mouse Functions
+    def mouse_pressed(self, button=0):
+        return self.current_mouse[button]
+
+
+    def mouse_pressed_once(self, button=0):
+        return (
+            self.current_mouse[button] and
+            not self.previous_mouse[button]
+        )
