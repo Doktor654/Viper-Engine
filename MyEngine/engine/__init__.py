43861e82e56762505601bcf5df8b54e88363c998
Doktor <74203521+Doktor654@users.noreply.github.com>
2026-04-20 13:32:26 +0200

    add initialization, removes all the thousands of imports
    add initialization, removes all the thousands of imports, can now use 'from engine import *'

---
+++ MyEngine/engine/__init__.py
@@ -0,0 +1,9 @@
+from engine.Scene import Scene
+from engine.Node import Node
+from engine.input import Input
+from engine.CollisionSystem import CollisionSystem
+from engine.game_loop import GameLoop
+from engine.Core.Signal import Signal
+from Renderer.renderer import Renderer
+
+from engine.Nodes import *
