12ff0b70074304a097dce96833ddabbb57e76647
Doktor <74203521+Doktor654@users.noreply.github.com>
2026-05-05 15:52:42 +0200

    Add automatic making of main file and main scene.

---
+++ MyEngine/viper_cli.py
@@ -0,0 +1,39 @@
+import sys
+import os
+
+MAIN_TEMPLATE = """from engine import *
+from GameScene import GameScene
+
+Game(GameScene, screen_size=(1280, 720), fps=60).run()
+"""
+
+SCENE_TEMPLATE = """from engine import *
+
+class GameScene(Scene):
+    def __init__(self, input):
+        super().__init__(input)
+        
+"""
+
+def main():
+    args = sys.argv[1:]
+
+    if not args or args[0] != "new" or len(args) < 2:
+        print("USAGE: viper new <ProjectName>")
+        return
+
+    name = args[1]
+
+    if os.path.exists(name):
+        print(f"Map '{name}' already exists!")
+        return
+
+    os.makedirs(f"{name}/assets")
+
+    with open(f"{name}/main.py", "w") as f:
+        f.write(MAIN_TEMPLATE)
+
+    with open(f"{name}/GameScene.py", "w") as f:
+        f.write(SCENE_TEMPLATE)
+
+    print(f"✅ Project '{name}' Created! Run: cd {name} && python main.py")
